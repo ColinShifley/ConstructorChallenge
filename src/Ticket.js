c8b49be8ac785c0fd2b3cbdc0213e600dd7c1353
Thomas Weldon <thomas@thedriven.dev>
2020-07-04 08:11:08 -0400

    Removed enterOrder function, set empty state for Table props

--- src/Ticket.js
+++ src/Ticket.js
@@ -1,40 +1,26 @@
 import React, { Component } from 'react';
 
-
 import Table1 from './Table1';
 
-
 class Ticket extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            protein: 'beef',
-            drink: '',
-            side: ''
-        };
+  constructor(props) {
+    super(props);
+    this.state = {
+      protein: '',
+      drink: '',
+      side: '',
     };
+  }
 
-    enterOrder() {
-        console.log('hello i work')
-        this.setState({
-            protein: 'beef',
-            drink: 'Coke',
-            side: 'Broccoli'
-        })
-    }
-
-        render()
-        {
-            return (
-                <div>
-                    <h2>
-                        <Table1 big={this.state.protein} takeOrder={this.enterOrder}/>
-                    </h2>
-                </div>
-            );
-        }
-    };
-
-
+  render() {
+    return (
+      <div>
+        <h2>
+          <Table1 big={this.state} />
+        </h2>
+      </div>
+    );
+  }
+}
 
 export default Ticket;
